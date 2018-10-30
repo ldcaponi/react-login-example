0c27df885577c83ac85560852329fac9e48e6f18
Lou Caponi <caplo04@ca.com>
2018-10-30 13:07:39 -0400

    Add styling for login form.

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,8 +1,56 @@
 import React from "react";
+import "./Login.scss";
 
 class Login extends React.Component {
+  state = { email: "", password: "" };
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
   render() {
-    return <div className="Login">Login Page</div>;
+    const { email, password } = this.state;
+    return (
+      <div className="Login">
+        <form className="LoginForm">
+          <h1 className="LoginForm__heading">Login</h1>
+          <div className="LoginForm__group">
+            <input
+              className="LoginForm__text-input"
+              value={email}
+              id="email"
+              name="email"
+              type="text"
+              placeholder="abc@123.com"
+              onChange={this.handleChange}
+            />
+            <label className="LoginForm__label" htmlFor="email">
+              Email
+            </label>
+          </div>
+
+          <div className="LoginForm__group">
+            <input
+              className="LoginForm__text-input"
+              value={password}
+              id="password"
+              name="password"
+              type="password"
+              placeholder="ABC123"
+              onChange={this.handleChange}
+            />
+            <label className="LoginForm__label" htmlFor="password">
+              Password
+            </label>
+          </div>
+
+          <div className="LoginForm__group">
+            <button className="LoginForm__submit-button" type="submit">
+              Login
+            </button>
+          </div>
+        </form>
+      </div>
+    );
   }
 }
 
