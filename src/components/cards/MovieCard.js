9832f5b07c245028d37e84e0b614093ce2aecf5b
Lou Caponi <caplo04@ca.com>
2018-10-30 15:04:09 -0400

    Fetching films, film card work in progress.

---
+++ src/components/cards/MovieCard.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./MovieCard.scss";
+
+const MovieCard = ({ film }) => {
+  return (
+    <div className="MovieCard">
+      <div className="MovieCard__image-container">
+        <img
+          className="MovieCard__image"
+          src={film.images.image[0].src}
+          alt="Movie"
+        />
+      </div>
+      <div className="MovieCard__title">{film.title}</div>
+    </div>
+  );
+};
+
+export default MovieCard;
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,20 +1,32 @@
 import React from "react";
+import MovieCard from "../cards/MovieCard";
 import { fetchFilms } from "../../services/DataService";
 import "./Home.scss";
 
 class Home extends React.Component {
+  state = {
+    films: []
+  };
   componentDidMount() {
     fetchFilms()
       .then(res => {
-        console.log(res.data.films);
+        this.setState({ films: res.data.films.film });
       })
       .catch(e => {});
   }
 
   render() {
+    const { films } = this.state;
+
     return (
       <div className="Home">
         <h1 className="Home__heading">Check out our latest films...</h1>
+
+        <div className="CardsContainer">
+          {films.map(i => (
+            <MovieCard key={i.id} film={i} />
+          ))}
+        </div>
       </div>
     );
   }
