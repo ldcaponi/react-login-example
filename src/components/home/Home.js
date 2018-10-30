c147eb6e10b5af7254b487dde7ed4965564dc772
Lou Caponi <caplo04@ca.com>
2018-10-30 14:36:15 -0400

    Add shell for Home page.

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,8 +1,22 @@
 import React from "react";
+import { fetchFilms } from "../../services/DataService";
+import "./Home.scss";
 
 class Home extends React.Component {
+  componentDidMount() {
+    fetchFilms()
+      .then(res => {
+        console.log(res.data.films);
+      })
+      .catch(e => {});
+  }
+
   render() {
-    return <div className="Home">Home Page</div>;
+    return (
+      <div className="Home">
+        <h1 className="Home__heading">Check out our latest films...</h1>
+      </div>
+    );
   }
 }
 
