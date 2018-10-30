3c53b996a6167627c99418f648cb7d8e35073892
Lou Caponi <caplo04@ca.com>
2018-10-30 15:20:57 -0400

    Added loading component.

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,32 +1,38 @@
 import React from "react";
 import MovieCard from "../cards/MovieCard";
+import Loader from "../loader/Loader";
 import { fetchFilms } from "../../services/DataService";
 import "./Home.scss";
 
 class Home extends React.Component {
   state = {
-    films: []
+    films: [],
+    loading: true
   };
   componentDidMount() {
     fetchFilms()
       .then(res => {
-        this.setState({ films: res.data.films.film });
+        this.setState({ films: res.data.films.film, loading: false });
       })
       .catch(e => {});
   }
 
   render() {
-    const { films } = this.state;
+    const { films, loading } = this.state;
 
     return (
       <div className="Home">
         <h1 className="Home__heading">Check out our latest films...</h1>
 
-        <div className="CardsContainer">
-          {films.map(i => (
-            <MovieCard key={i.id} film={i} />
-          ))}
-        </div>
+        {!loading && (
+          <div className="CardsContainer">
+            {films.map(i => (
+              <MovieCard key={i.id} film={i} />
+            ))}
+          </div>
+        )}
+
+        {loading && <Loader />}
       </div>
     );
   }
