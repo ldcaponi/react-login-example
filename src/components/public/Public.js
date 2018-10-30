f0483c354e8c52f4efbd2f0a47b6676515a2f9ec
Lou Caponi <caplo04@ca.com>
2018-10-30 13:23:03 -0400

    Add mixins and styling for public page.

--- src/components/public/Public.js
+++ src/components/public/Public.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./Public.scss";
 
 const Public = props => {
-  return <div className="Public">Public Page</div>;
+  return (
+    <div className="Public">
+      <div className="CenterBox">
+        <h1 className="Public__heading">Welcome to our site!</h1>
+        <p className="Public__paragraph">
+          Please <Link to="/login">log in</Link> to continue
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Public;
